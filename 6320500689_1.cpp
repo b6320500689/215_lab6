b4d3be5c8a1e8ef122a5a3ec44ada085628e0670
b6320500689 <supitchaya.r@ku.th>
2021-03-18 15:06:44 +0700

    11111111111111111

--- 6320500689_1.cpp
+++ 6320500689_1.cpp
@@ -2,13 +2,21 @@
 #include <string.h>
 int main ()
 {
-	int n,i;
+	int n,i,j;
 	scanf ("%d",&n);
 	char a[n][100];
+	char b[100][100];
 	for (i=0; i<n; i++)
 	{
 		scanf ("%s",a[i]);
-		printf ("%s\n",a[i]);
+		
 	}
-	
+	for (i=0; i<n; i++)
+	{
+		if (a[i][0]>90)
+			a[i][0]-=32;
+	}
+	printf ("\n");
+	printf ("%s\n",a[i]);
+	return 0;
 }
