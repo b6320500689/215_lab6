d720f37d18db0fc649629e9e0e08fcddfbee6e37
b6320500689 <supitchaya.r@ku.th>
2021-03-18 16:34:07 +0700

    33333

--- 6320500689_3.cpp
+++ 6320500689_3.cpp
@@ -3,5 +3,32 @@ int main()
 {
 	long i,j,k,n,q,max;
 	scanf("%ld %ld",&n,&q);
+	char a[n];
+	long c[n]={};
+	long x[q],y[q];
 	
+	for (i=0; i<q; i++)
+	{
+		scanf ("%c %ld %ld",&a[i],&x[i],&y[i]);
+	}
+	for (i=0; i<q; i++)
+	{
+		if (a[i] == 'U')
+		{
+			c[x[i]] = y[i];
+		}
+		else if (a[i] == 'P')
+		{
+			max = c[x[i]];
+			for (j=x[i]; j<=y[i]; j++)
+			{
+				if (max < c[j])
+				{
+					max = c[j];
+				}
+			}
+			printf ("%ld\n",max);
+		}
+	}
+	return 0;
 }
