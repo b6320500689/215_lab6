0cef7969248cd1031f3ff93a969631c763a514c5
b6320500689 <supitchaya.r@ku.th>
2021-03-18 14:53:31 +0700

    1111

--- 6320500689_1.cpp
+++ 6320500689_1.cpp
@@ -2,12 +2,13 @@
 #include <string.h>
 int main ()
 {
-	int n;
+	int n,i;
 	scanf ("%d",&n);
 	char a[n][100];
 	for (i=0; i<n; i++)
 	{
 		scanf ("%s",a[i]);
+		printf ("%s\n",a[i]);
 	}
-	printf ("%s\n",a[i]);
+	
 }
