70681085d77ce72ce2b3134644e19715fb9cdaa7
b6320500689 <supitchaya.r@ku.th>
2021-03-18 16:09:12 +0700

    2

---
+++ 6320500689_2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+	int n,i,j,x=0,y=0;
+	scanf ("%d",&n);
+	char a[2][n];
+	if (n>=4 && n<=9)
+	{
+		for (i=0; i<2; i++)
+		{
+			scanf ("%[^\n]s",a[i]);
+		}
+		for (j=0; j<n; j++)
+		{
+			if (a[0][j] == a[1][j])
+				x++;
+			else
+				y++;
+		}
+		printf ("%d %d",x,y);
+	}
+}
